8546cad906d3e1efbd6e0a33f2296674ec2bb484
french-coral <166878262+french-coral@users.noreply.github.com>
2025-04-01 12:05:13 +0200

    Add files via upload

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import time
 import board
 import usb_cdc
+import random
 from adafruit_neotrellis.neotrellis import NeoTrellis
 from adafruit_neotrellis.multitrellis import MultiTrellis
 
@@ -98,28 +99,35 @@ class TrellisManager:
             else:
                 self.set_led(x, y, OFF)   # Éteint si déjà allumé
 
-    def initialize_board(self):
+    def initialize_board(self,step:str):
         """
         Initialisation des LEDs avec un effet dégradé, miam miam
         """
+        if step == "menu":
+            couleur_gradient = 5
+        else: #Current else : "init"
+            couleur_gradient = 32
+
+
         for y in range(8):
             for x in range(8):
                 self.trellis.activate_key(x, y, NeoTrellis.EDGE_RISING)
                 self.trellis.set_callback(x, y, self.handle_button_test) # associe la fonction handle quand le (x,y) est cliqué
 
                 # Applique un dégradé
-                gradient_color = (x * 32, y * 32, 150)
+                gradient_color = (x * couleur_gradient, y * couleur_gradient, 150)
                 self.set_led(x, y, gradient_color)
-                time.sleep(0.05)
+                time.sleep(0.01)
 
         # Éteint toutes les LEDs après l'effet de démarrage
-        time.sleep(0.5)
+        time.sleep(0.05)
         for y in range(8):
             for x in range(8):
                 self.set_led(x, y, OFF)
-                time.sleep(0.02)
+                time.sleep(0.01)
 
     def benchmark(self,x,y):
+        """Benchmark temp pour mapper le board"""
         if self.get_led_status(x,y) != OFF:
             self.set_led(x,y,OFF)
 
@@ -132,10 +140,61 @@ class TrellisManager:
         if edge == NeoTrellis.EDGE_RISING:
             if (x, y) in [(1,1), (1,2)]:  # Si on appuie sur la partie "Solo"
                 self.menu_type = 'Solo'
-                self.initialize_board()
+                self.initialize_board("menu")
+                self.solo_start()
             elif (x, y) in [(5,1), (5,2), (6,1), (6,2)]:  # Si on appuie sur "Duo"
                 self.menu_type = 'Duo'
             print(f"Mode sélectionné : {self.menu_type}")
+
+
+    def placer_bateaux(self):
+        """
+        Place aléatoirement 3 bateaux sur la grille.
+        Retourne une liste de tuples représentant avec positions des bateaux.
+        """
+        grille = [[0] * 8 for n in range(8)]  # Grille vide 8x8 (0 = libre ; 1 = occupé)
+        tailles_bateaux = [4, 3, 2]  # (Grand, Moyen, Petit)
+        bateaux_places = []  # Liste des bateaux avec leurs positions
+
+        for taille in tailles_bateaux:
+            IsBateau_placed = False
+
+            while not IsBateau_placed:
+                x = random.randint(0, 7)
+                y = random.randint(0, 7)
+                direction = random.choice(["H", "V"])  # Horizontal ou Vertical en gros
+
+
+                # Vérifier si le bateau peut être placé
+                positions = []
+                if direction == "H" and x + taille <= 8:  # Horizontal touche t il le bord ?
+                    for i in range(taille):
+                        positions.append((x + i, y))# Pose horizontal
+                elif direction == "V" and y + taille <= 8:  # Vertical, même chose
+                    for i in range(taille):
+                        positions.append((x, y + i))# Pose vertical
+
+                if not positions: # si le bateau depasse du plateau on retry
+                    continue
+
+                IsBateau_placable = True
+                for x,y in positions:
+                    if grille[x][y] == 1: # déjà occupé
+                        IsBateau_placable = False
+                        break
+                if IsBateau_placable:
+                    for x, y in positions:
+                        grille[x][y] = 1
+                    bateaux_places.append(positions)# enregistre les coordonnées valides
+                    IsBateau_placed = True
+
+        return bateaux_places
+
+    def solo_start(self):
+        bots_bateaux = self.placer_bateaux()
+        for bateau in bots_bateaux:
+            print(bateau)  # Affiche les coordonnées des bateaux choisi par le bot
+
 
 
     def menu(self):
@@ -160,7 +219,7 @@ class TrellisManager:
 
 # Création et initialisation du gestionnaire
 manager = TrellisManager(trellis)
-manager.initialize_board()
+manager.initialize_board("init")
 manager.menu()
 
 #manager.benchmark(0,1)
